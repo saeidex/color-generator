d16a5c2a555a30fd3f69887a5ef87d93fdb94f97
EK Nayeem <saeidex.dev@gmail.com>
2024-01-14 23:02:37 +0600

    feat: add color code display

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import "./style.css";
 /**
  * Project Recuirements:
- * - Change the backgroud color by generating random rgb color by clicking a button
+ * - Change the backgroud color by generating random HEX color by clicking a button and show color code
  */
 
 // onloader handler
@@ -13,20 +13,22 @@ function main() {
   // collecting references
   const root = document.getElementById("root");
   const changeBtn = document.getElementById("change-btn");
+  const displayColorCode = document.getElementById("display-color-code");
 
   // handle click event
   changeBtn.addEventListener("click", function () {
     root.classList.remove("bg-cyan-400");
     const bgColor = generateRgbColor();
     root.style.backgroundColor = bgColor;
+    displayColorCode.value = bgColor;
   });
 }
 
-// randor color generator
+// color generator function
 function generateRgbColor() {
   const red = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
 
-  return `rgb(${red}, ${green}, ${blue})`;
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
